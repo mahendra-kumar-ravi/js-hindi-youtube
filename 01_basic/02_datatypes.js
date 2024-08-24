7ab9f43e87b2cd05e0f5fde43294ead85e0589d5
mahendra-kumar-ravi <mahendravits7@gmail.com>
2024-08-24 12:09:38 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all code as newer version
+//alert(3+3) // we are using node.js not browser
+
+console.log("mahi")
+// maek sure code is readable
+
+let name = "mahi"
+let ate = 20
+let isLoggedIn = false;
+
+
+// number 2 to ki power 53
+// bigint 
+// string == ""
+// boolean => true/false
+// null => stand alone value
+// undefined
+// symbol
+
+//object
+
+console.log(typeof "mahi");
+
+console.log(typeof undefined);   // undefined
+console.log(typeof null) // object
+
+
+
+
+  
