db653497116808d615951828e7b0c59e305cada3
mahendra-kumar-ravi <mahendravits7@gmail.com>
2024-08-24 07:07:00 +0000

    variables decleration in javascript

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12322
+let accountEmail = "mahi@gmail.com"
+var accountPass ="123456"
+accountCity = "jaiput"
+let accountState;
+
+// accountId =3;
+
+accountEmail = "mohan@gmail.com"
+accountPass = "32132132"
+accountCity = "jodhpur"
+
+console.table([accountEmail, accountPass, accountCity, accountId, accountState])
+
+/* predder not to use var 
+because of the scope */
