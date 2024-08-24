03f13340cd9c2a6688413ea9f6d482c2d3febdcb
mahendra-kumar-ravi <mahendravits7@gmail.com>
2024-08-24 13:18:29 +0000

    operations on datatypes

--- 01_basic/conversionOperation.js
+++ 01_basic/conversionOperation.js
@@ -6,4 +6,41 @@ let valueInNumber = Number(score)
 
 //console.log(valueInNumber);
 
- 
+ //******* operations *******//
+
+ let value = 3;
+ let negvalue = -value 
+//  console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " mahi"
+
+let str3 = str1+str2;
+// console.log(str3);
+
+
+// console.log("1" +2);
+// console.log(1 + "2");
+// console.log("1" + 2 +2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+4;
+// console.table([num1, num2, num3]);
+
+let counter =100;
+++counter;
+console.log(counter);
+
+
