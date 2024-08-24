144f5a1cd059e4af02f692dbb369d7432be3ecde
mahendra-kumar-ravi <mahendravits7@gmail.com>
2024-08-24 13:33:08 +0000

    comparision in js

---
+++ 01_basic/comparision.js
@@ -0,0 +1,18 @@
+// console.log("3" > 1);
+// console.log("02" >1);
+
+// console.log (null> 0);
+// console.log(null == 0);
+// console.log(null >= 0)
+
+// console.log(null === 0) // false
+//  console.log(undefined ==0);
+//  console.log(undefined > 0);
+//  console.log(undefined < 0);
+
+ 
+ console.log("2" === 2);
+ 
+ 
+
+
