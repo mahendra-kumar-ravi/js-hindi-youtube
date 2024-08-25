af06c0c3524be7afbb663d30eadb44e26b3833f2
mahendra-kumar-ravi <mahendravits7@gmail.com>
2024-08-25 11:03:16 +0000

    string mehtods

---
+++ 01_basic/05_string.js
@@ -0,0 +1,33 @@
+const name = "mahi "
+const repoCount = 50
+
+// console.log(name + repoCount + "value");
+
+// console.log(`hello my nmae is ${name} and repo count is ${repoCount} `);
+
+const gameName = new String('mahendra')
+
+// console.log(gameName[2]);
+
+// console.log(gameName.charAt(4));
+// console.log(gameName.toLocaleUpperCase());
+
+// console.log(gameName.indexOf('n'));
+
+// const newString = gameName.substring(1,6);
+// console.log(newString);
+
+const newStringone = "   mahendra  ";
+// console.log(newStringone);
+
+// console.log (newStringone.trim());
+
+const url = "https://hitesh.com/hites%20chaudhari"
+
+// console.log(url.replace('%20', '-'));
+
+
+
+
+
+
