1858a577057bfedc80ec77260a7f7a6bac5f2fbc
mahendra-kumar-ravi <mahendravits7@gmail.com>
2024-08-25 16:06:33 +0000

    dates and time

---
+++ 01_basic/07_dates.js
@@ -0,0 +1,32 @@
+//Dates
+
+let myDate = new Date()
+
+// console.log(myDate.toString());
+
+// console.log(myDate.toDateString() + "  toDateString");
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toTimeString());
+
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// console.log(myCreatedDate.toDateString());
+
+let myCreatedDate = new Date("01-17-2025")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimestamp = Date.now()
+// console.log(myTimestamp)
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
+
+
+
