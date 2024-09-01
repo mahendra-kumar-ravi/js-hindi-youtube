82cd70309ef11d8f2178751ea3ddc08bf37a6345
mahendra-kumar-ravi <mahendravits7@gmail.com>
2024-09-01 18:10:30 +0000

    Array part 1

---
+++ 02_basic/01_array.js
@@ -0,0 +1,40 @@
+ const myarr = [1,2,3,4,5,]
+const myHeros = ["hanuman", "saktiman"]
+
+// console.log(myarr[4]);
+// console.log(myarr[0]);
+//array method 
+// myarr.push(6)
+// console.log(myarr);
+// myarr.push(33);
+// console.log(myarr);
+// myarr.pop()
+// console.log(myarr);
+// myarr.unshift(22)
+// console.log(myarr)
+// myarr.unshift(44);
+// console.log(myarr)
+// myarr.shift()
+// console.log(myarr)
+
+// const myArr2 = new Array(1,2,3,4);
+// console.log(myarr);
+// // console.log(myarr.includes(3));
+// // console.log(myarr.indexOf(3))
+
+// const newArr = myarr.join()
+// console.log(newArr)
+// console.log(typeof(newArr))
+
+//sloice, spice
+
+console.log("A ", myarr);
+
+const myn1 = myarr.slice(1,3)
+console.log(myn1);
+console.log("B ", myarr);
+
+const myn2 = myarr.splice(1,3)
+console.log(myn1);
+console.log(myn2);
+console.log("c ", myarr);
